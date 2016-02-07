6ce58ecd9631ebc3366f7ea2f99e2489e4e00ad8
Nicolas MUGNIER <mugnier.nicolas@gmail.com>
2016-02-07 18:18:08 +0100

    calculate custom view size according to button size (use constants)

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -13,6 +13,8 @@ class RatingControl: UIView {
     // MARK: Properties
     var rating = 0
     var ratingButtons = [UIButton]()
+    let spacing = 5
+    let stars = 5
     
     // MARK: Initialization
     required init?(coder aDecoder: NSCoder) {
@@ -20,7 +22,7 @@ class RatingControl: UIView {
         super.init(coder: aDecoder)
         
         for _ in 0..<5 {
-            let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+            let button = UIButton()
             button.backgroundColor = UIColor.redColor()
             button.addTarget(self, action: "ratingButtonTapped:", forControlEvents: .TouchDown)
             ratingButtons += [button]
@@ -30,16 +32,25 @@ class RatingControl: UIView {
     }
     
     override func layoutSubviews() {
-        var buttonFrame = CGRect(x: 0, y: 0, width: 44, height: 44)
+        
+        // Set the button's width and height to a square the size of the frame's height.
+        let buttonSize = Int(frame.size.height)
+        
+        var buttonFrame = CGRect(x: 0, y: 0, width: buttonSize, height: buttonSize)
         // Offset each button's origin by the length of the button plus spacing.
         for (index, button) in ratingButtons.enumerate() {
-            buttonFrame.origin.x = CGFloat(index * (44 + 5))
+            buttonFrame.origin.x = CGFloat(index * (buttonSize + spacing))
             button.frame = buttonFrame
         }
     }
     
     override func intrinsicContentSize() -> CGSize {
-        return CGSize(width: 240, height: 44)
+        
+        let buttonSize = Int(frame.size.height)
+        let width = (buttonSize + spacing) * stars
+        
+        return CGSize(width: width, height: buttonSize)
+        
     }
     
     // MARK: Button Action
