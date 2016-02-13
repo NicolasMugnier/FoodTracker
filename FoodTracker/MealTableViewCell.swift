bc9ea610d42bd5820dd24c5912e5ebf557c988f6
Nicolas MUGNIER <mugnier.nicolas@gmail.com>
2016-02-13 11:22:14 +0100

    Add table view controller as default entry and create custom table cell

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Nicolas MUGNIER on 13/02/2016.
+//  Copyright © 2016 Nicolas MUGNIER. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
