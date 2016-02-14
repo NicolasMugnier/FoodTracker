f7d4fbfe368ab6fede28f3c28c84662218eb206d
Nicolas MUGNIER <mugnier.nicolas@gmail.com>
2016-02-14 14:43:51 +0100

    edit an existing meal

--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -67,9 +67,21 @@ class MealTableViewController: UITableViewController {
     @IBAction func unwindToMealList(sender: UIStoryboardSegue){
         
         if let sourceViewController = sender.sourceViewController as? MealViewController, meal = sourceViewController.meal {
-            let newIndexPath = NSIndexPath(forRow: meals.count, inSection: 0)
-            meals.append(meal)
-            tableView.insertRowsAtIndexPaths([newIndexPath], withRowAnimation: .Bottom)
+            
+            if let selectedIndexPath = tableView.indexPathForSelectedRow {
+            
+                // Update an existing meal.
+                meals[selectedIndexPath.row] = meal
+                tableView.reloadRowsAtIndexPaths([selectedIndexPath], withRowAnimation: .None)
+            
+            }else{
+                
+                let newIndexPath = NSIndexPath(forRow: meals.count, inSection: 0)
+                meals.append(meal)
+                tableView.insertRowsAtIndexPaths([newIndexPath], withRowAnimation: .Bottom)
+                
+            }
+            
         }
         
     }
